c166dfbb985f6cb0252a0e72f8c5df0705d790b2
hjw4595 <hongjung73@gmail.com>
2019-08-29 23:17:40 +0900

    nodeMailer test

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,14 @@ export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
+
+export const sendMail = (email) => null;
+
+export const sendSecretMail = (adress, secret) =>{
+    const email ={
+        from : "hongjung73@gmail.com",
+        to: adress,
+        subject: "login for insta",
+        html: `key is ${secret}<br>capy paste on the app</br>`
+    }
+}
